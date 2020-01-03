c8001e67ba2b38d825fba4048139183a97206507
tenasatupitsyn <214782@bk.ru>
2020-01-03 10:43:50 -0300

    Validators now known as resolvers

--- components/Resolver.js
+++ components/Resolver.js
@@ -1,13 +1,13 @@
 const { parse } = require('url')
 
-module.exports = class Validators {
-	static FilesValidator(...files) {
+module.exports = class Resolver {
+	static resolveFiles(...files) {
 		return Array.prototype.concat.apply([], files)
 			.filter((file) => !file.includes(' '))
 			.map((file) => parse(file).slashes ? parse(file).path.slice(1) : file)
 	}
 
-	static AlbumValidator(short) {
+	static resolveAlbum(short) {
 		short = parse(short).path
 		return short.slice(0, 3) === '/c/' ? short.slice(3) : short
 	}
--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-	Validators: require('./Validators')
+	Resolver: require('./Resolver')
 }
